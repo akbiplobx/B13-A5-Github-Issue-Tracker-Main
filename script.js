a4166a127adececd923c60b82da36c4af09daf26
A K Biplob <akbiplobx@gmail.com>
2026-03-07 19:49:15 +0600

    toggle

--- script.js
+++ script.js
@@ -9,3 +9,85 @@ function login() {
     }
 }
 // login function end
+// ================================================================================
+const container = document.getElementById("cardContainer");
+
+let allIssues = [];
+
+// API load
+fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+  .then(res => res.json())
+  .then(data => {
+
+    allIssues = data.data; // API থেকে issues
+    displayCards(allIssues);
+
+  });
+//   ======================
+
+function displayCards(cards){
+
+  container.innerHTML = "";
+
+  cards.forEach(card => {
+
+    const div = document.createElement("div");
+
+      const statusImg = card.status === "open" 
+      ? "./image/Open-Status.png"
+      : "./image/Closed-Status.png";
+
+
+    div.innerHTML = `
+    <div class="bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow flex flex-col h-full">
+      <div class="p-4 flex flex-col flex-grow">
+
+        <div class="flex justify-between items-start mb-2">
+          <div class="w-7 h-7 rounded-full bg-emerald-50 flex items-center justify-center text-emerald-500 text-xs">
+            <img src="${statusImg}" alt="">
+          </div>
+
+          <span class="text-[10px] font-bold px-2 py-0.5 rounded bg-red-50 text-red-500 uppercase">
+            ${card.priority}
+          </span>
+        </div>
+
+        <h3 class="text-sm font-bold text-gray-800 leading-tight mb-1">
+          ${card.title}
+        </h3>
+
+        <p class="text-xs text-gray-500 line-clamp-2 mb-3">
+          ${card.description}
+        </p>
+
+        <div class="mt-auto pt-3 border-t border-gray-50 text-[10px] text-gray-400">
+          <p>#${card.id} by ${card.author}</p>
+          <p>${card.createdAt}</p>
+        </div>
+
+      </div>
+    </div>
+    `;
+
+    container.appendChild(div);
+
+  });
+}
+
+
+// button functions
+
+function showAll(){
+  displayCards(allIssues);
+}
+
+function showOpen(){
+  const filtered = allIssues.filter(card => card.status === "open");
+  displayCards(filtered);
+}
+
+function showClosed(){
+  const filtered = allIssues.filter(card => card.status === "closed");
+  displayCards(filtered);
+}
+// ===================================================================================
